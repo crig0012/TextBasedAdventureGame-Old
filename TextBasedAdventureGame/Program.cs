ef4359306743e12191d44afce7e368bb712bdf20
crig0012 <crig0012@algonquinlive.com>
2013-11-25 20:57:39 -0500

    First Commit, Added Framework

---
+++ TextBasedAdventureGame/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextBasedAdventureGame
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool playing = true;
+            Game theGame = new Game();
+            theGame.reset();
+            while (playing == true)
+            {
+                theGame.checkIfValid(Console.ReadLine());
+            }
+        }
+    }
+
+    public class Game
+    {
+        bool beenHereWater = false;
+        bool lookedAround = false;
+        List<string> inventory = new List<string>();
+
+        public void reset()
+        {
+            inventory.Add("EndOfInv");
+
+            beenHereWater = false;
+            lookedAround = false;
+        }
+
+        public void checkIfValid(string toCheck)
+        {
+            whereTo(toCheck);
+        }
+
+        private void whereTo(string toHere)
+        {
+            switch (toHere)
+            {
+                case "Water":
+                    waterStuff();
+                    break;
+
+                case "Look around":
+                    LookAround();
+                    break;
+
+                case "Test Inventory":
+                    testInventory();
+                    break;
+
+                case "Inventory":
+                    printInventory();
+                    break;
+            }
+        }
+
+        private void printInventory()
+        {
+            foreach (string inventoryItem in inventory)
+            {
+                Console.WriteLine(inventoryItem);
+            }
+        }
+
+        private void waterStuff()
+        {
+            foreach(string inventoryItem in inventory)
+            {
+                if (inventoryItem == "Rope")
+                {
+                    Console.WriteLine("You look around warily, fearing the rapids. You look around your bag for something to assist in crossing, and find a rope. It takes a coupld of tries, but you manage to hook the rope on a tough rock, and cross safely");
+                    beenHereWater = true;
+                    inventory.Remove("Rope");
+                    return;
+                }
+
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (beenHereWater == true)
+            {
+                Console.WriteLine("Having been here already, you cockily attempt to traverse the rapids. Your quick steps lead to your downfall");
+                reset();
+            }
+
+            else
+            {
+                Console.WriteLine("You gaze around in wonder. Having never seen such quick rapids before, so you take your time crossing. Unfortunately, taking your time means you were in the way when a tree came down, knocking you into the wtaer, where you freeze/drown/get clubbed by a tree to death.");
+            }
+        }
+
+        private void testInventory()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                inventory.Add("Item " + i);
+            }
+        }
+
+        private void inventoryFull(string itemToGet)
+        {
+            Console.WriteLine("Your inventory is full. You'll have to drop an item to pick up that " + itemToGet + ".");
+            foreach (string inventoryItem in inventory)
+            {
+                Console.WriteLine(inventoryItem);
+            }
+
+            string thingToDrop = Console.ReadLine();
+
+            foreach (string inventoryItem in inventory)
+            {
+                if (inventoryItem == thingToDrop)
+                {
+                    inventory.Remove(thingToDrop);
+                    inventory.TrimExcess();
+                }
+
+                else
+                {
+                    continue;
+                }
+            }
+        }
+
+        private void LookAround()
+        {
+            if (lookedAround == false)
+            {
+                Console.WriteLine("You look around, and find a rope on the ground nearby!");
+                foreach (string inventoryItem in inventory)
+                {
+                    if (inventoryItem == null)
+                    {
+                        inventory.Add("Rope");
+                        lookedAround = true;
+                        return;
+                    }
+
+                    else if (inventoryItem == "EndOfInv")
+                    {
+                        inventoryFull("Rope");
+                        lookedAround = true;
+                        return;
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("You've already looked around. There is nothing interesting anymore");
+            }
+        }
+    }
+}
